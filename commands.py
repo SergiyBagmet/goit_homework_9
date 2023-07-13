cff819ebd07d659af6931a1086e7ab9435093292
SergiyBagmet <134723713+SergiyBagmet@users.noreply.github.com>
2023-07-14 00:56:48 +0300

    try func add

--- commands.py
+++ commands.py
@@ -1,13 +1,26 @@
+from collections import defaultdict,namedtuple
+
+#TODO вибрать способ записи данних
+User = namedtuple("User",['name','phone'])
+users = list()
+
+
 def hello(*_):
     print("How can I help you?")
 
 def add_name_phone(name, phone) :
+    # TODO санитайзер номера??? проверка коректности!
+    user = User(name, phone)
+       
     
-    print(f"name : {name}\nphone : {phone}" )
+def change_phone(name, phone):
+    pass
+    
 
 BOT_COMMANDS = {
     "hello": hello,
     "add": add_name_phone,
+    "change": change_phone,
 }
 
 BOT_EXIT = ["good bye", "close", "exit"]
--- console_bot.py
+++ console_bot.py
@@ -1,18 +1,20 @@
-
 from commands import *
 from parser_sanitayz import *
 
-def main():
+
+def main(): 
     while True:
         user_input = input("Enter command: ")
         command, *user_info = parser(user_input)
-        
+            
         if command in BOT_EXIT:
             print("Good bye!")
             break
 
         do_command = BOT_COMMANDS[command]
         do_command(*user_info)
+  
+
 
 if __name__ == '__main__':
-    main()
+   main()
